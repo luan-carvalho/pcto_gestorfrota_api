418ba17c3d005915cf4da8ae8cb8e0863d199de8
luan-carvalho <carvalho.luan@pm.me>
2025-11-25 10:33:39 -0300

    repo init

---
+++ src/main/java/br/edu/ifto/gestorfrotaapi/GestorfrotaapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.gestorfrotaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorfrotaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorfrotaapiApplication.class, args);
+	}
+
+}
