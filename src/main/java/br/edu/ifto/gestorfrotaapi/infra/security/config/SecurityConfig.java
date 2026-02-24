9f00790bc6b00d15042d0ffde0fbf35a0b8696df
luan-carvalho <55687321+luan-carvalho@users.noreply.github.com>
2026-02-24 17:31:23 -0300

    allowing cors

--- src/main/java/br/edu/ifto/gestorfrotaapi/infra/security/config/SecurityConfig.java
+++ src/main/java/br/edu/ifto/gestorfrotaapi/infra/security/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.edu.ifto.gestorfrotaapi.infra.security.filter.SecurityFilter;
 
@@ -56,6 +58,20 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:[*]")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
