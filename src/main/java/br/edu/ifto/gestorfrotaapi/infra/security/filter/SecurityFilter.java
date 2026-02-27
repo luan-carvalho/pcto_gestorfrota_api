ae95824badb0e2706a947e1a4378c2bbbbdd01f3
luan-carvalho <55687321+luan-carvalho@users.noreply.github.com>
2026-02-27 00:36:26 -0300

    b

--- src/main/java/br/edu/ifto/gestorfrotaapi/infra/security/filter/SecurityFilter.java
+++ src/main/java/br/edu/ifto/gestorfrotaapi/infra/security/filter/SecurityFilter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.edu.ifto.gestorfrotaapi.infra.security.service.TokenService;
@@ -17,6 +18,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
